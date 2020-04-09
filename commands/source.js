8ee76cc3ae02619d5d82a9a18968f1e0d1167e37
hax4dayz <Uitvinder.gdm@gmail.com>
2020-04-09 20:25:42 +0200

    Adding source command

---
+++ commands/source.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'source',
+	description: 'Displays the source of the bot',
+	execute(message, args) {
+        const Discord = require('discord.js');
+		const client = new Discord.Client();
+		if (message.author.bot) return;
+		message.channel.send(`You can find my source at https://github.com/hax4dazy/Komet-JS. Serious PRs and issues welcome!`);
+	},
+};
