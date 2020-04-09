65d1105e2501f61188c8dc31fcc42d9ffcf5b3c6
hax4dayz <Uitvinder.gdm@gmail.com>
2020-04-10 00:14:53 +0200

    Fixing version and adding .komet command for credits. I'm kometing this :shrek:

--- Index.js
+++ Index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const { MessageEmbed } = require('discord.js');
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-global.version = '4.2.0'
+global.version = '1.0.0'
 
 const {	prefix, 
 	Token,
