91046a9181e31717aec4e5eba23949252a1abefe
hax4dayz <Uitvinder.gdm@gmail.com>
2020-04-25 00:12:54 +0200

    yes

--- Index.js
+++ Index.js
@@ -4,9 +4,9 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const { MessageEmbed } = require('discord.js');
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const {prefix, Token, SetStatusCommand} = require ('./config.json')
-const {BotLog, MessageLog, RequirePermissions, ModLog} = require('./info.json')
-const {BootSuccessful} = require('./strings.json')
+const {prefix, Token, SetStatusCommand} = require ('./config.json');
+const {BotLog, MessageLog, RequirePermissions, ModLog} = require('./info.json');
+const {BootSuccessful} = require('./strings.json');
 global.version = '1.0.0'
 
 
@@ -225,76 +225,3 @@ client.on('messageDelete', async message => {
 	}
 });
 
-client.on('guildMemberUpdate', (oldMember, newMember) => {
-	const guild = newMember.guild;
-	// declare changes
-	var Changes = {
-	  unknown: 0,
-	  addedRole: 1,
-	  removedRole: 2,
-	  username: 3,
-	  nickname: 4,
-	  avatar: 5
-	}
-	var change = Changes.unknown
-  
-	// check if roles were removed
-	var removedRole = ''
-	oldMember.roles.every(function (value) {
-	  if (newMember.roles.find('id', value.id) == null) {
-		change = Changes.removedRole
-		removedRole = value.name
-	  }
-	})
-  
-	// check if roles were added
-	var addedRole = ''
-	newMember.roles.every(function (value) {
-	  if (oldMember.roles.find('id', value.id) == null) {
-		change = Changes.addedRole
-		addedRole = value.name
-	  }
-	})
-  
-	// check if username changed
-	if (newMember.user.username != oldMember.user.username) {
-	  change = Changes.username
-	}
-	// check if nickname changed
-	if (newMember.nickname != oldMember.nickname) {
-	  change = Changes.nickname
-	}
-	// check if avatar changed
-	if (newMember.user.avatarURL != oldMember.user.avatarURL) {
-	  change = Changes.avatar
-	}
-	// post in the guild's log channel
-	var log = guild.channels.find('name', CHANNEL)
-	if (log != null) {
-	  switch (change) {
-		case Changes.unknown:
-		  log.sendMessage('**[User Update]** ' + newMember)
-		  break
-		case Changes.addedRole:
-		  log.sendMessage('**[User Role Added]** ' + newMember + ': ' + addedRole)
-		  break
-		case Changes.removedRole:
-		  log.sendMessage('**[User Role Removed]** ' + newMember + ': ' + removedRole)
-		  break
-		case Changes.username:
-		  log.sendMessage('**[User Username Changed]** ' + newMember + ': Username changed from ' +
-			oldMember.user.username + '#' + oldMember.user.discriminator + ' to ' +
-			newMember.user.username + '#' + newMember.user.discriminator)
-		  break
-		case Changes.nickname:
-		  log.sendMessage('**[User Nickname Changed]** ' + newMember + ': ' +
-			(oldMember.nickname != null ? 'Changed nickname from ' + oldMember.nickname +
-			  +newMember.nickname : 'Set nickname') + ' to ' +
-			(newMember.nickname != null ? newMember.nickname + '.' : 'original username.'))
-		  break
-		case Changes.avatar:
-		  log.sendMessage('**[User Avatar Changed]** ' + newMember)
-		  break
-	  }
-	}
-  });
