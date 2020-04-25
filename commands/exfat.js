a09b8dcfa227dca6567da7e00e76e30db5e39d07
DanielC-08 <thetechgeekgamer@gmail.com>
2020-04-25 02:10:27 -0700

    exfat

---
+++ commands/exfat.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'bsfat',
+	description: 'Yes',
+	execute(message, args) {
+		message.channel.send(':regional_indicator_e: :regional_indicator_x: :regional_indicator_f: :regional_indicator_a: :regional_indicator_t:');
+	},
+};
