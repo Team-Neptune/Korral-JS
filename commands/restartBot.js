59147cd8498bf9c94dd3a2cbb0ec815eaf187d5b
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-05-13 09:53:59 -0700

    Added restart command

---
+++ commands/restartBot.js
@@ -0,0 +1,27 @@
+module.exports = {
+	name: 'restart',
+	description: 'Kills the bot',
+	aliases: ['kill', 'reboot', 'die'],
+	usage: '',
+	cooldown: 0,
+	staff:true,
+	essential:true,
+	execute(message, args, client) {
+		try{
+		const { MessageEmbed } = require('discord.js')
+		const RestartedEmbed = new Discord.MessageEmbed()
+		RestartedEmbed.setTitle('🔄 Restarting')
+		RestartedEmbed.setDescription('Bye :wave:')
+		message.channel.send(RestartedEmbed)
+		setTimeout(function(){ 
+			process.exit()
+		}, 2000);
+	}catch(error) {
+		respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
+		errorlog(error)
+		// Your code broke (Leave untouched in most cases)
+		console.error('an error has occured', error);
+		}
+	}
+	
+};
