25b8a689d46166545b2d05aba1a0e56079c84dad
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-05-12 23:52:36 -0700

    Fix bam command

--- commands/bam.js
+++ commands/bam.js
@@ -7,7 +7,7 @@ module.exports = {
     const client = new Discord.Client();
     const fs = require('fs');
     const argarray = message.content.slice(prefix.length).trim().split(/ +/g);
-    const user = message.mentions.users.first() || message.guild.members.cache.get(`${argarray[1]}`)|| client.users.cache.get('name', `${argarray[1]}`) ||message.author;
-    message.channel.send( `${user.tag}`+ ' is ̶n͢ow b̕&̡.̷ 👍̡!');
+    const user = message.mentions.users.first() || message.guild.members.cache.get(`${args[0]}`)|| client.users.cache.get('name', `${args.join(' ')}`) ||message.author;
+    message.channel.send( `${user.tag}`+ ' is ̶n͢ow b̕&̡.̷ 👍̡');
     },
 };
