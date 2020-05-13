bd9bf1be6ddc441cd7f76503ed50bb30d5beb8a2
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-05-13 09:01:07 -0700

    Added speak command

---
+++ commands/speak.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'say',
+  aliases: ['speak'],
+  description: 'Has the bot speak in the channel it is ran in.',
+  usage: '<text>',
+  cooldown: 0,
+  staff:true,
+	execute(message, args, client) {	
+    message.delete()
+		const { prefix } = require('../config.json');;
+		const text = args.join(' ');
+    message.channel.send(text)
+  }}
