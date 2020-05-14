58479a824c7d6393c9013fdb365c8180d917b47b
hax4dayz <Uitvinder.gdm@gmail.com>
2020-05-14 23:47:09 +0200

    I change

--- commands/guide.js
+++ commands/guide.js
@@ -5,6 +5,6 @@ module.exports = {
         const Discord = require('discord.js');
 		const client = new Discord.Client();
 		if (message.author.bot) return;
-		message.channel.send(`Generic starter guides:\n Beginners Guide: https://switch.homebrew.guide/\n\nSpecific guides:\nManually Updating/Downgrading (with HOS): https://switch.homebrew.guide/usingcfw/manualupgrade\nManually Repairing/Downgrading (without HOS): https://switch.homebrew.guide/usingcfw/manualchoiupgrade`);
+		message.channel.send(`**Generic starter guides:**\nBeginners Guide: https://switch.homebrew.guide/\n\n**Specific guides:**\nManually Updating/Downgrading (with HOS): https://switch.homebrew.guide/usingcfw/manualupgrade\nManually Repairing/Downgrading (without HOS): https://switch.homebrew.guide/usingcfw/manualchoiupgrade`);
 	},
 };
