1a4267393b87255900161c25d796c5a5743681c0
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-05-13 19:10:37 -0700

    Bot messages won't be caught in message edit detection

--- komet.js
+++ komet.js
@@ -206,6 +206,7 @@ from ${message.author.tag} (${message.author.id}), in <#${message.channel.id}>:
 
 //Log message edits
 client.on('messageUpdate', (oldMessage, newMessage) => {
+	if(newMessage.author != client.user)
 	newMessage.guild.channels.cache.get(modLog).send(`:pencil: Message edit: 
 from ${newMessage.author.tag} (${newMessage.author.id}), in <#${newMessage.channel.id}>:
 \`${oldMessage.content}\` → \`${newMessage.content}\``)
