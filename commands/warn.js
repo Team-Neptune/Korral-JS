8355762fab3b8fd82da12d74855d08dc0ed2e341
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-05-15 21:30:05 -0700

    Added fully working warn command

--- commands/warn.js
+++ commands/warn.js
@@ -7,6 +7,15 @@ module.exports = {
     execute(message, args) {
 		const fs = require('fs');
 		try {
+			var userlog = require('../warnings.json')
+			if(!userlog[`${message.author.id}_warnings`]){
+				userlog[`${message.author.id}_warnings`] = 0
+				let data = JSON.stringify(userlog);
+				fs.writeFile('./warnings.json', data, (err) => {console.log(err)})
+		}
+			delete require.cache[require.resolve(`../warnings.json`)]
+
+
 			if (message.author.id == message.mentions.members.first().id){message.channel.send(`You can't perform this action on yourself.`);return;}
 			const {staffRoles} = require('../config.json');
 			if (message.mentions.members.first().roles.cache.some(role => staffRoles.includes(role.id))){
@@ -19,31 +28,22 @@ module.exports = {
 			var reason = args.join(' ').replace(args[0], '')
 			if(reason == ''){var reason = 'No reason provided.'}
 
-			amountofwarns = 0
-			if(fs.existsSync('./warnings.json')){
-			var warningjson = require('../warnings.json')
-			if(warningjson.warnings)amountofwarns = warningjson.warnings
-			delete require.cache[require.resolve(`../warnings.json`)]
-			}
-			let student = { 
-				name: `${user.tag}`, 
-				reason: `${reason}`,
-				warnings:amountofwarns+1
 
-			};
-			 
-			let data = JSON.stringify(student, null, 2);
+				userlog[`${message.author.id}_warn${userlog[`${message.author.id}_warnings`]+1}`] = reason;
+				userlog[`${message.author.id}_warnings`] = userlog[`${message.author.id}_warnings`]+1;
+
+			let data = JSON.stringify(userlog);
 			
 			fs.writeFile('./warnings.json', data, (err) => {
 				if (err) throw err;
 				console.log('Data written to file');
 				delete require.cache[require.resolve(`../warnings.json`)]
-				warningjson = require('../warnings.json')
+				userlog = require('../warnings.json')
 			delete require.cache[require.resolve(`../warnings.json`)]
-			warningjson = require('../warnings.json')
-			message.mentions.members.first().send(`You were warned on ${message.guild.name}. The given reason is: ${reason}\n\nPlease read the rules. This is warn #${warningjson.warnings}.`)
-			message.channel.send(`<@${user.id}> warned. User has ${warningjson.warnings} warning(s).`)
-			message.guild.channels.cache.get(modLog).send(`:warning: Warned: <@${message.author.id}> warned <@${message.mentions.members.first().id}> (warn #${warningjson.warnings}) | ${message.mentions.members.first().user.tag}
+			userlog = require('../warnings.json')
+			message.mentions.members.first().send(`You were warned on ${message.guild.name}. The given reason is: ${reason}\n\nPlease read the rules. This is warn #${userlog[`${message.author.id}_warnings`]}.`)
+			message.channel.send(`<@${user.id}> warned. User has ${userlog[`${message.author.id}_warnings`]} warning(s).`)
+			message.guild.channels.cache.get(modLog).send(`:warning: Warned: <@${message.author.id}> warned <@${message.mentions.members.first().id}> (warn #${userlog[`${message.author.id}_warnings`]}) | ${message.mentions.members.first().user.tag}
 :pencil2: Reason: "${reason}"`)
 delete require.cache[require.resolve(`../warnings.json`)]
 });
