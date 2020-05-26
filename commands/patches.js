d1dd2527693f996328ff5cea05e3be41f3e95a84
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-05-26 13:21:30 -0700

    Patches command

---
+++ commands/patches.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name:"es",
+    description: 'Warns a user.',
+    aliases: ['patches'],
+    execute(message, args, client){
+        if(!args[0]){
+            message.channel.send(`For pirated eshop-games you need ES signature patches. As their only purpose is to allow piracy we're not providing any help with installation of said patches or pirated games afterwards`)
+        }else if(args[0] == '--yes'){
+            message.channel.send(`Patches? You want 🩹?  Ohhhhhh you mean you want like the patches patches that patch stuff for switch.. hmmmmmmmm 🤔 why do u need these patches.. idc, or do i.... 🤷‍♂️ ok kbye i guess`)
+        }else if(args[0] == '--hax')[
+            message.channel.send(`:canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: :canned_food: `)
+        ]
+    }
+}
