8fefcce56bd10827c641c98a877223146294525f
Daniel <46201432+DanielC-08@users.noreply.github.com>
2020-05-28 22:27:28 -0700

    Updated warm command and added cool command

--- commands/warm.js
+++ commands/warm.js
@@ -2,10 +2,25 @@ module.exports = {
     name: 'warm',
     description: 'warms the user',
     execute(message, args) {
-    const Discord = require('discord.js');
-    const client = new Discord.Client();
-    const fs = require('fs');
-    const user = message.mentions.users.first() || message.author;
-    message.channel.send( `<@${user.id}> ` + 'warmed. User is now ' + Math.ceil(Math.random() * 80) + '°C');
+        if (!fs.existsSync('./memeValues.json')){
+            fs.writeFileSync('./memeValues.json', '{}')
+            warmMember()
+        }else{
+            warmMember()
+        }
+        function warmMember(){
+            const user = message.mentions.users.first() || message.author;
+                memeValues = require('../memeValues.json')
+                if(!memeValues[`${user.id}warm`]){
+                    memeValues[`${user.id}warm`] =  Math.ceil(Math.random() * 80)
+                }else{
+                    memeValues[`${user.id}warm`] = memeValues[`${user.id}warm`] + Math.ceil(Math.random() * 80)
+                    fs.writeFile('./memeValues.json', JSON.stringify(memeValues), (err) => {
+                        if(err)console.log(err)
+                    })
+                }
+                message.channel.send( `<@${user.id}> ` + 'warmed. User is now ' + memeValues[`${user.id}warm`] + `°C (${Math.ceil((memeValues[`${user.id}warm`] * 1.8)+32)}°F, ${Math.ceil((memeValues[`${user.id}warm`] + 273.15))}K)`);
+        }
+
     },
 };
