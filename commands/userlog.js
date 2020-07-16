54d65fe21c8eba24c8e5523812f89d3cfa7222e0
Daniel <46201432+DanielC-08@users.noreply.github.com>
2020-07-16 14:52:50 -0700

    Multiple user mentions possible on userinfo and userlog command

--- commands/userlog.js
+++ commands/userlog.js
@@ -10,34 +10,35 @@ module.exports = {
 		}
 
 		if (message.mentions.members.first()) {
-			var mentionedUser = message.mentions.members.first();
 		} else {
 			returnResponse(`No user was mentioned.`);
 			return;
 		}
 
 		// all requirements are met
-
 		var warnings = require('../warnings.json')
 		var notes = require('../userNotes.json')
 
-		if (!warnings[mentionedUser.id] && !notes[mentionedUser.id]) {
-			returnResponse(`This user does not have any warnings or notes.`);
-			return;
-		}
-
-		const embed = new Discord.MessageEmbed()
-		if(warnings[mentionedUser.id]){
-			warnings[mentionedUser.id].forEach(function (warning, index) {
-				embed.addField('Warning: ' + (parseInt(index) + 1), warning)
-			});
-		}
-		if(notes[mentionedUser.id]){
-			notes[mentionedUser.id].forEach(function (warning, index) {
-				embed.addField('Note: ' + (parseInt(index) + 1), warning)
-			});
-		}
-		message.channel.send(embed)
+		message.mentions.members.forEach(mentionedUser => {
+			if (!warnings[mentionedUser.id] && !notes[mentionedUser.id]) {
+				returnResponse(`${mentionedUser.user.tag} does not have any warnings or notes.`);
+				return;
+			}
+	
+			const embed = new Discord.MessageEmbed()
+			embed.setAuthor(mentionedUser.user.tag)
+			if(warnings[mentionedUser.id]){
+				warnings[mentionedUser.id].forEach(function (warning, index) {
+					embed.addField('Warning: ' + (parseInt(index) + 1), warning)
+				});
+			}
+			if(notes[mentionedUser.id]){
+				notes[mentionedUser.id].forEach(function (warning, index) {
+					embed.addField('Note: ' + (parseInt(index) + 1), warning)
+				});
+			}
+			message.channel.send(embed)
+		});
 
 		delete require.cache[require.resolve(`../warnings.json`)]
 		delete require.cache[require.resolve(`../userNotes.json`)]
