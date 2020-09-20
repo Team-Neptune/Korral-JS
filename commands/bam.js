8c41ade136f1fbe9867a1d89320d211b4ec6b10c
FennecTECH <fennectech@gmail.com>
2020-09-20 14:20:08 -0500

    Update bam.js

--- commands/bam.js
+++ commands/bam.js
@@ -8,6 +8,6 @@ module.exports = {
     const fs = require('fs');
     const argarray = message.content.slice(prefix.length).trim().split(/ +/g);
     const user = message.mentions.users.first() || message.guild.members.cache.get(`${args[0]}`)|| client.users.cache.get('name', `${args.join(' ')}`) ||message.author;
-    message.channel.send( `${user.tag}`+ ' is ̶n͢ow b̕&̡.̷ 👍̡');
+    message.channel.send( `${user.tag}`+ ' is ̶n͢ow :b:&̡.̷ 👍̡');
     },
 };
