60b892363c03271d0f353dfc0537eb10c793991e
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-01-02 11:51:19 -0800

    Update korral.js

--- korral.js
+++ korral.js
@@ -1,24 +1,18 @@
 console.log('Loading, please wait a moment.')
 
-fs = require('fs')
+const fs = require('fs')
 fetch = require("node-fetch");
 
-Discord = require('discord.js');
-client = new Discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client({ws:{intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]}});
 client.commands = new Discord.Collection();
-commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const {
-	MessageEmbed
-} = require('discord.js');
 
-const {
-	prefix,
-	token,
-	botLog,
-	modLog,
-	userLog,
-	staffRoles
-} = require('./config.json');
+const commands = require(`./commands/${file}`)
+commands.forEach(command => {
+	client.commands.set(command.name, command);
+	console.log(`exported ${command.name}`)
+})
+
 const config = require('./config.json')
 
 
@@ -27,102 +21,30 @@ respond = function (title, content, sendto, color) {
 	//He doesn't check to make sure it works on this bot :shrek:
 	sendto.send(content)
 }
-/*
-const { exec } = require("child_process");
-exec("git rev-parse HEAD", (error, stdout, stderr) => {
-	if(stderr)return;
-	commitID = `${stdout}`
-	version = `${stdout}`
-	if(stderr)console.log(stderr)
-	})
-exec("git log master --format=\"%H\" -n 1", (err, stdout, stderr) => {
-	if(stderr)return;
-	latestCommit = stdout
-	if(stderr)console.log(stderr)
-})
-exec("git log master -1 --pretty=%B", (err, stdout, stderr) => {
-	if(stderr)return;
-	latestCommitMessage = stdout
-	if(stderr)console.log(stderr)
-})
-
-//Check for updates automatically
-autoCheckForUpdates = function(){
-	console.log('Checking for updates...')
-	setTimeout(function(){ 
-		exec("git rev-parse HEAD", (error, stdout, stderr) => {
-			if(stderr)return;
-			commitID = `${stdout}`
-			version = `${stdout}`
-			})
-		exec("git log master --format=\"%H\" -n 1", (err, stdout, stderr) => {
-			latestCommit = stdout
-		})
-		exec("git log master -1 --pretty=%B", (err, stdout, stderr) => {
-			latestCommitMessage = stdout
-		})
-		if(latestCommit != commitID){
-			const UpdateAvailableEmbed = new Discord.MessageEmbed()
-			.setTitle('Update Available')
-			.setColor('ffa500')
-			.setDescription(`An update is available.\nLatest commit ID: ${latestCommit}\nLocal commit ID: ${commitID}`)
-			.addField('Commit message',latestCommitMessage,false)
-			.setTimestamp()
-			.setFooter(`${client.user.username} | Version: ${commitID}`)
-			client.channels.cache.get(`${botLog}`).send(UpdateAvailableEmbed);
-			console.log('Checking for updates complete. Update was found.')
-		}else{
-			console.log('Checking for updates complete. No update found.')
-		}
-		autoCheckForUpdates()
-	}, 43200000);
-}
-autoCheckForUpdates()
-
-*/
 
 //Required files
 let requiredFiles = ["warnings.json", "userNotes.json"]
 for (let index = 0; index < requiredFiles.length; index++) {
 	const element = requiredFiles[index];
-	if(!fs.existsSync('./'+element)){
-		fs.writeFileSync('./'+element, JSON.stringify({}))
+	if(!fs.existsSync(`./${element}`)){
+		fs.writeFileSync(`./${element}`, JSON.stringify({}))
 	}
 }
 
 //Bootup check
 client.once('ready', () => {
-	commitID = "2.0.0"
 	console.log('Ready!');
-	//console.log(`Local commit ID: ${commitID}`)
-	//	console.log(`Latest commit ID: ${latestCommit}`)
 	const StartupEmbed = new Discord.MessageEmbed()
 		.setColor('#00FF00')
 		.setTitle('Bot Started')
 		.setTimestamp()
-		.setFooter(`${client.user.username} | Version: ${commitID}`)
+		.setFooter(`${client.user.username}`)
 	client.channels.cache.get(`${botLog}`).send(StartupEmbed);
-	//Check for updates
-	return;
-	if (latestCommit != commitID) {
-		const UpdateAvailableEmbed = new Discord.MessageEmbed()
-			.setTitle('Update Available')
-			.setColor('ffa500')
-			.setDescription(`An update is available.\nLatest commit ID: ${latestCommit}\nLocal commit ID: ${commitID}`)
-			.addField('Commit message', latestCommitMessage, false)
-			.setTimestamp()
-			.setFooter(`${client.user.username} | Version: ${commitID}`)
-		client.channels.cache.get(`${botLog}`).send(UpdateAvailableEmbed);
-
-	}
 })
 
-process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
-
-//Error
-client.on('error', error => {
-	console.error('an error has occured', error);
-
+process.on('unhandledRejection', error => {
+	console.error('Uncaught Promise Rejection', error)
+	client.channels.cache.get(config.botl)
 });
 
 //Login
@@ -170,98 +92,24 @@ client.on('message', message => {
 //Member join
 client.on('guildMemberAdd', member => {
 	if(config.userLogging == false)return;
-	const guild = member.guild
-	member.guild.channels.cache.get(`${userLog}`).send(`:white_check_mark: Join: <@${member.id}> | ${member.user.tag}
-:calendar_spiral: Creation: ${member.user.createdAt}
-:label: User ID: ${member.id}
-:hash: Server Member Count: ${member.guild.memberCount}`)
+	member.guild.channels.cache.get(`${userLog}`).send(`:white_check_mark: Join: <@${member.id}> | ${member.user.tag}\n:calendar_spiral: Creation: ${member.user.createdAt}\n:label: User ID: ${member.id}\n:hash: Server Member Count: ${member.guild.memberCount}`)
 }
 );
 
 //Member leave
 client.on('guildMemberRemove', member => {
 	if(config.userLogging == false)return;
-	const guild = member.guild
-	client.channels.cache.get(`${userLog}`).send(`:arrow_left: Leave: <@${member.id}> | ${member.user.tag}
-:label: User ID: ${member.id}
-:hash: Server Member Count: ${member.guild.memberCount}`)
+	client.channels.cache.get(`${userLog}`).send(`:arrow_left: Leave: <@${member.id}> | ${member.user.tag}\n:label: User ID: ${member.id}\n:hash: Server Member Count: ${member.guild.memberCount}`)
 });
 
 //Log deleted messages
 client.on('messageDelete', async message => {
-	const fetchedLogs = await message.guild.fetchAuditLogs({
-		limit: 1,
-		type: 'MESSAGE_DELETE',
-	});
-
-	message.guild.channels.cache.get(modLog).send(`:wastebasket: Message delete: 
-from ${message.author.tag} (${message.author.id}), in <#${message.channel.id}>:
-\`${message.content}\``)
-	//May fix it up someday TM
-	return;
-	// Since we only have 1 audit log entry in this collection, we can simply grab the first one
-	const deletionLog = fetchedLogs.entries.first();
-
-	// Let's perform a sanity check here and make sure we got *something*
-	if (!deletionLog) {
-		console.log(`A message by ${message.author.tag} was deleted, but no relevant audit logs were found.`);
-		const DeletionEmbed = new Discord.MessageEmbed()
-			.setColor('#ff0000')
-			.setTitle('Message Deleted')
-			.addFields(
-				{ name: 'Message sent by', value: message.author.tag, inline: false },
-				{ name: 'Deleted by', value: 'Unknown - Audit log was not found.', inline: false },
-				{ name: 'Sent in', value: message.channel.name, inline: false },
-				{ name: 'Message', value: message.content, inline: false },
-			)
-			.setTimestamp()
-		client.channels.cache.get(`${modLog}`).send(DeletionEmbed)
-	}
-
-	// We now grab the user object of the person who deleted the message
-	// Let us also grab the target of this action to double check things
-	const { executor, target } = deletionLog;
-
-
-	// And now we can update our output with a bit more information
-	// We will also run a check to make sure the log we got was for the same author's message
-	if (target.id === message.author.id) {
-		console.log(`A message by ${message.author.tag} was deleted by ${executor.tag}.`)
-		const DeletionEmbed = new Discord.MessageEmbed()
-			.setColor('#ff0000')
-			.setTitle('Message Deleted')
-			.addFields(
-				{ name: 'Message sent by', value: message.author.tag, inline: false },
-				{ name: 'Deleted by', value: executor.tag + ` (${executor.id})`, inline: false },
-				{ name: 'Sent in', value: message.channel.name, inline: false },
-				{ name: 'Message', value: message.content, inline: false },
-			)
-			.setTimestamp()
-		client.channels.cache.get(`${modLog}`).send(DeletionEmbed)
-		return;
-	} else {
-		if (target.id === message.author.id) return;
-		console.log(`A message by ${message.author.tag} was deleted, but we don't know by who.`)
-		const DeletionEmbed = new Discord.MessageEmbed()
-			.setColor('#ff0000')
-			.setTitle('Message Deleted')
-			.addFields(
-				{ name: 'Message sent by', value: message.author.tag, inline: false },
-				{ name: 'Deleted by', value: 'Unable to find.', inline: false },
-				{ name: 'Sent in', value: message.channel.name, inline: false },
-				{ name: 'Message', value: message.content, inline: false },
-			)
-			.setTimestamp()
-		client.channels.cache.get(`${modLog}`).send(DeletionEmbed)
-		return;
-	}
+	message.guild.channels.cache.get(modLog).send(`:wastebasket: Message delete: \nfrom ${message.author.tag} (${message.author.id}), in <#${message.channel.id}>:\n\`${message.content}\``)
 });
 
 //Log message edits
 client.on('messageUpdate', (oldMessage, newMessage) => {
-	if (newMessage.author.bot) return
-	if(newMessage.content == oldMessage.content)return;
-	if (newMessage.author != client.user)
+	if(oldMessage.aut)
 		newMessage.guild.channels.cache.get(modLog).send(`:pencil: Message edit: 
 from ${newMessage.author.tag} (${newMessage.author.id}), in <#${newMessage.channel.id}>:
 \`${oldMessage.content}\` → \`${newMessage.content}\``)
