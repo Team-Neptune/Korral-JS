c67e5ff21a9a01093dc7387eacc515b675da27f0
FennecTECH <fennectech@gmail.com>
2021-04-26 13:18:17 -0500

    Update exfat.js
    Fix URL

--- commands/exfat.js
+++ commands/exfat.js
@@ -6,7 +6,7 @@ module.exports = {
         const client = new Discord.Client();
         const exampleEmbed = new Discord.MessageEmbed()
         .setTitle("Guiformat")
-        .setURL('http://www.ridgecrop.demon.co.uk/guiformat.exe')
+        .setURL('http://ridgecrop.co.uk/guiformat.exe')
         .setDescription('A useful tool for formatting SD cards over 32GB as FAT32 on Windows.')
         message.channel.send("The exFAT drivers built into the Switch has been known to corrupt SD cards and homebrew only makes this worse. Backup everything on your SD card as soon as possible and format it to FAT32. On Windows, if your SD card is over 32GB then it will not let you select FAT32 from the built-in format tool, however you can use a tool like GUIFormat to format it.")
         message.channel.send(exampleEmbed)
