6cf151b72dfe2098c6d2bbdc1426b0ee8d13487b
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-05-14 00:23:09 -0700

    Create config-template.ts

---
+++ config-template.ts
@@ -0,0 +1,13 @@
+import { Config } from "./src/info";
+
+export const config:Config = {
+	"prefix": ["", ""],
+	"token": "",
+	"botLog": "",
+	"modLog": "",
+	"userLog":"",
+	"suspiciousWordsLog":"",
+	"suspiciousWordsFilter":true,
+	"userLogging":true,
+	"staffRoles":["", ""]
+}
