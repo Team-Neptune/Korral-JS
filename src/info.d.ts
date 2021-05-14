01881af4a0c6110239fd721b353f2d8d9318f8ab
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-05-14 01:03:22 -0700

    Create info.d.ts

---
+++ src/info.d.ts
@@ -0,0 +1,30 @@
+import {Message} from 'discord.js'
+export interface Command {
+  /** Command name */
+  name:string,
+  description:string,
+  usage?:string,
+  aliases?:Array<string>
+  cooldown?:number,
+  staffOnly:boolean,
+  allowedChannels?:Array<string>,
+  execute(message:Message, args:Array<String>)
+}
+
+export interface Config {
+  "prefix": Array<string>,
+  "token": string,
+  "botLog": string,
+  "modLog": string,
+  "userLog":string,
+  "suspiciousWordsLog":string,
+  "suspiciousWordsFilter":boolean,
+  "userLogging":boolean,
+  "staffRoles":Array<string>
+}
+
+declare module 'discord.js' {
+    interface Client {
+      commands: Collection<string, Command>
+    }
+}
