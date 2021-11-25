5fa670d10fd10d9fc70d8fad8325464510f2e83e
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-11-25 13:22:43 -0800

    Add button commands

---
+++ src/buttons/close_ticket_.ts
@@ -0,0 +1,34 @@
+import { TextChannel } from "discord.js";
+import { config } from "../../config";
+import ButtonCommand from "../classes/ButtonCommand";
+
+export default new ButtonCommand({
+    checkType:"STARTS_WITH",
+    execute(interaction){
+        let ticketUserId = interaction.customId.split("close_ticket_")[1];
+        let currentUserId = interaction.member.user.id;
+        let threadChannelId = interaction.channelId;
+        if(currentUserId != ticketUserId && (interaction.member.roles as string[]).find(roleId => config.staffRoles.includes(roleId) || config.supportRoleId == roleId))
+            return interaction.reply({
+                content:`You can't close a ticket that isn't yours.`,
+                ephemeral:true
+            })
+        interaction.reply({
+            content:`<#${threadChannelId}> will be locked soon.`,
+            ephemeral:true
+        })
+        .then(() => {
+            (interaction.client.channels.cache.get(threadChannelId) as TextChannel).send({
+                embeds:[
+                    {
+                        "description":`🔒 Ticket has been closed by <@${currentUserId}>`,
+                        "color":16711680
+                    }
+                ]
+            })
+            .then(() => {
+                return interaction.client.closeSupportThread(threadChannelId, ticketUserId)
+            })
+        })
+    }
+})
