b55a6614e72cf3f67cbd4bc929748e00864b9108
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-11-30 23:40:13 -0800

    Specify guides are not created by Team Neptune

--- src/commands/guide.ts
+++ src/commands/guide.ts
@@ -3,7 +3,7 @@ import Command from "../classes/Command";
 export default new Command({
     execute(interaction){
         interaction.reply({
-            content:`**Generic starter guides:**\n[Beginners Guide](https://switch.homebrew.guide/)\n\n**Specific guides:**\n[Manually Updating/Downgrading (with HOS)](https://switch.homebrew.guide/usingcfw/manualupgrade)\n[Manually Repairing/Downgrading (without HOS)](https://switch.homebrew.guide/usingcfw/manualchoiupgrade)\n[Creating an emuMMC](https://switch.homebrew.guide/emummc/emummc.html)`
+            content:`**Generic starter guides:**\n[Beginners Guide](https://switch.homebrew.guide/)\n\n**Specific guides:**\n[Manually Updating/Downgrading (with HOS)](https://switch.homebrew.guide/usingcfw/manualupgrade)\n[Manually Repairing/Downgrading (without HOS)](https://switch.homebrew.guide/usingcfw/manualchoiupgrade)\n[Creating an emuMMC](https://switch.homebrew.guide/emummc/emummc.html)\n\n**These guides were not created by Team Neptune.**`
         })
     }
 })
