e8c4501c5abea39bfdef9e51d80aa73b642472d7
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2022-01-20 17:10:49 -0800

    Update moderation.ts

--- src/msg_commands/moderation.ts
+++ src/msg_commands/moderation.ts
@@ -52,45 +52,5 @@ export const moderationCommands:MessageCommand[] = [
           message.delete()
           message.channel.send(args.join(" "))
         }
-    },
-    {
-        name: 'echo',
-        description: 'Has the bot speak in another channel.',
-        usage: '<channel> <text>',
-        cooldown: 0,
-        staffOnly:true,
-        execute(message, args) {          
-            if(!message.mentions.channels.first())
-                return message.channel.send('Please mention a channel.')
-            var removed = args.splice(0, 1)
-            let text = args.join(' ');
-            (message.mentions.channels.first() as TextChannel).send(text)
-        }
-    },
-    {
-        name: 'userlogid',
-        description: 'Views the user warn log',
-        staffOnly: true,
-        execute(message, args) {
-            if (message.mentions.members.first())
-                return message.channel.send(`Please use userlog command instead.`);
-    
-            if (!existsSync(`./warnings.json`))
-                return message.channel.send(`'warnings.json' doesn't exist. Please do at least one warning to create the file.`)
-    
-            // all requirements are met
-            var warnings = require('../../warnings.json')
-    
-            if (!warnings[(args[0] as unknown as number)])
-                return message.channel.send(`This user does not have any userlog entries`);
-    
-            const embed = new MessageEmbed()
-            warnings[(args[0] as unknown as number)].forEach(function (warning, index) {
-                embed.addField('Warning: ' + (parseInt(index) + 1), warning)
-            });
-            message.channel.send({embeds:[embed]})
-    
-            delete require.cache[require.resolve(`../../warnings.json`)]
-        }
     }
 ]
