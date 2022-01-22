15d7d4df724c8c91e33dbdfae1937818c6b11dbe
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2022-01-22 13:46:15 -0800

    Remove leftover code in purge, shrek, speak

--- src/commands/shrek.ts
+++ src/commands/shrek.ts
@@ -1,8 +1,4 @@
 import Command from "../classes/Command";
-import { Message, TextChannel } from "discord.js";
-const message_guild_id = "703301751171973190"
-const message_channel_id = "703302552594284594"
-const message_id = "824322420529823764"
 
 export default new Command({
     commandName:"shrek",
