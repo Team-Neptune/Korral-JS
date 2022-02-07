13e4e355bb1ed23be7cce25cd5a35dbf7fa9497f
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2022-02-06 18:39:18 -0800

    Squashed commit of the following:
    commit 185a93ff78d51a341c262266fa88dd36a499e068
    Author: Daniel <46201432+TechGeekGamer@users.noreply.github.com>
    Date:   Sun Feb 6 18:25:19 2022 -0800
    
        Update meme eta messages

--- src/commands/eta.ts
+++ src/commands/eta.ts
@@ -18,9 +18,12 @@ export default new Command({
             "when deepsea becomes a cake",
             "when daniel is banned",
             "when daniel become good programmer",
+            "when daniel gets a good computer",
+            "when daniel plays good games",
+            "when daniel stops making unstable bots",
             "when hax stops playing apex",
             "when hax stops golfing",
-            "when hax finally [explains](https://cdn.discordapp.com/attachments/744457183843844147/934593115623419904/unknown.png)"
+            "when hax finally [explains](https://cdn.discordapp.com/attachments/744457183843844147/934593115623419904/unknown.png) his message from <t:1606257486:R>"
         ]
         const index = Math.floor(Math.random() * messages.length)
         const msg = messages[index]
