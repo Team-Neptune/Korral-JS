a55b5656db95c845f331f5cfd59e5578250a5717
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2022-02-11 18:31:23 -0800

    Update open_ticket.ts
    Update modal text

--- src/buttons/open_ticket.ts
+++ src/buttons/open_ticket.ts
@@ -31,8 +31,7 @@ export default new ButtonCommand({
                   custom_id: 'question1',
                   style: 1,
                   label: 'Firmware + Atmosphere / DeepSea version',
-                  placeholder: 'FW 13.2.1, Atmosphere 1.2.6, DeepSea 3.5.0',
-                  min_length:12,
+                  placeholder: 'FW X.X.X, Atmosphere X.X.X, DeepSea X.X.X',
                   max_length:50
                 }
               ]
@@ -44,8 +43,8 @@ export default new ButtonCommand({
                   type: 4,
                   custom_id: 'question2',
                   style: 1,
-                  label: 'Do you use Hekate or Fusee-Primary?',
-                  placeholder: 'Hekate / Fusee Primary',
+                  label: 'Do you use Hekate or Fusee?',
+                  placeholder: 'Hekate / Fusee',
                   min_length:5,
                   max_length:50
                 }
@@ -58,10 +57,8 @@ export default new ButtonCommand({
                   type: 4,
                   custom_id: 'question3',
                   style: 1,
-                  label: 'Do you have an error code/screen?',
-                  placeholder: 'Error Code 0000-0000 / Send screenshot when ticket opened',
-                  min_length:8,
-                  max_length:50,
+                  label: 'Do you have an error code and screen?',
+                  placeholder: 'Error Code 0000-0000 / Screenshot link',
                   required:false
                 }
               ]
@@ -74,7 +71,7 @@ export default new ButtonCommand({
                   custom_id: 'question4',
                   style: 1,
                   label: 'Coming for support with SDSetup or DeepSea?',
-                  placeholder: 'SDSetup / DeepSea / Other',
+                  placeholder: 'SDSetup / DeepSea / Something else',
                   max_length:50
                 }
               ]
