edb415dc7df82aaa64e3b7298e090d19a7b805ad
pyrocumulus0 <55328223+pyrocumulus0@users.noreply.github.com>
2022-05-11 11:33:08 +0530

    typo fix (#26)

--- src/commands/patches.ts
+++ src/commands/patches.ts
@@ -5,7 +5,7 @@ export default new Command({
     subCommandGroup:"switch",
     execute(interaction){
         interaction.reply({
-            content:`For pirated eshop-games, forwarders and other unofficial stuff you need signature patches. You can download them seperatly via the included "Switch AIO updater" homebrew. As their general purpose is to allow piracy we're not providing any help with installation or problems of said patches or pirated games afterwards.`
+            content:`For pirated eshop-games, forwarders and other unofficial stuff you need signature patches. You can download them separatly via the included "Switch AIO updater" homebrew. As their general purpose is to allow piracy, we're not providing any help with installation or problems of said patches or pirated games afterwards.`
         })
     }
 })
